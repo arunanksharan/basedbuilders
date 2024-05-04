532a2c9705d14c18cfcc3404e894d5d63a793b9a
“John <johnswaroop28@gmail.com>
2024-05-04 12:21:54 +0530

    Recast module added

--- basedbuilderbot/src/app/utils/bot.ts
+++ basedbuilderbot/src/app/utils/bot.ts
@@ -1,11 +1,22 @@
-import { NeynarAPIClient, isApiErrorResponse } from '@neynar/nodejs-sdk';
-import { AxiosError } from 'axios';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
+import { NeynarAPIClient, isApiErrorResponse } from "@neynar/nodejs-sdk";
+import { AxiosError } from "axios";
 
 // Instantiate the client
+console.log(process.env.NEYNAR_API_KEY);
 const client = new NeynarAPIClient(process.env.NEYNAR_API_KEY as string); // Replace with your Neynar API Key.
 const SIGNER = process.env.SIGNER as string;
 
-client.publishCast(SIGNER, 'hello builders', { channelId: 'basedbuilders' });
+export const gm = async () => {
+  let res = await client.publishCast(SIGNER, "gm", {
+    channelId: "basedbuilders",
+  });
+  return res;
+};
+
+export const recast = async (castHash: string) => {
+  let res = await client.publishReactionToCast(SIGNER, "recast", castHash);
+  if (!res.success) {
+    return false;
+  }
+  return true;
+};
