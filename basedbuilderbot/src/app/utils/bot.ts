878c0ab318b42bbb7a485470a13717dbad3c240d
“John <johnswaroop28@gmail.com>
2024-05-06 20:42:21 +0530

    template updated

--- basedbuilderbot/src/app/utils/bot.ts
+++ basedbuilderbot/src/app/utils/bot.ts
@@ -1,4 +1,4 @@
-import { NeynarAPIClient, isApiErrorResponse } from '@neynar/nodejs-sdk';
+import { NeynarAPIClient, isApiErrorResponse } from "@neynar/nodejs-sdk";
 
 const NEYNAR_API_KEY = process.env.NEYNAR_API_KEY;
 const SIGNER_KEY = process.env.SIGNER;
@@ -8,14 +8,14 @@ const client = new NeynarAPIClient(NEYNAR_API_KEY as string); // Replace with yo
 const SIGNER = SIGNER_KEY as string;
 
 export const gm = async () => {
-  let res = await client.publishCast(SIGNER, 'gm', {
-    channelId: 'basedbuilders',
+  let res = await client.publishCast(SIGNER, "gm", {
+    channelId: "basedbuilders",
   });
   return res;
 };
 
 export const recast = async (castHash: string) => {
-  let res = await client.publishReactionToCast(SIGNER, 'recast', castHash);
+  let res = await client.publishReactionToCast(SIGNER, "recast", castHash);
   if (!res.success) {
     return false;
   }
@@ -27,24 +27,24 @@ export const castToChannel = async (
   embeds?: { url: string }[]
 ) => {
   let config: {} = {
-    channelId: 'basedbuilders',
+    channelId: "basedbuilders",
   };
 
   if (embeds && embeds.length > 0) {
     config = { ...config, embeds };
   }
 
-  let messageTemplate = `${message.text}\n Posted by: @${message.username}`;
+  let messageTemplate = `${message.text}\nCasted by: @${message.username}`;
 
   let res = await client.publishCast(SIGNER, messageTemplate, config);
-  console.log('Cast to channel response:', res);
+  console.log("Cast to channel response:", res);
   return res;
 };
 
 export const process_message = (message: string) => {
-  let result = message.replace(/@basedbuilders/g, '');
-  result = result.replace(/@undefined/g, '').trim();
-  result = result.replace('Description', '').trim();
-  console.log('Processed message:', result);
+  let result = message.replace(/@basedbuilders/g, "");
+  result = result.replace(/@undefined/g, "").trim();
+  result = result.replace("Description", "").trim();
+  console.log("Processed message:", result);
   return result;
 };
