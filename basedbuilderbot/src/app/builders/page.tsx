e131f6dcf0bf40794d9cc848676969ced64e6f0f
“John <johnswaroop28@gmail.com>
2024-05-15 13:47:38 +0530

    builder build fix

--- basedbuilderbot/src/app/builders/page.tsx
+++ basedbuilderbot/src/app/builders/page.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import BuilderCard from "@/components/builderCard/builderCard";
 
 const fetchProfiles = async () => {
@@ -7,32 +8,40 @@ const fetchProfiles = async () => {
   return res;
 };
 
-async function Builders() {
-  let builders = (await fetchProfiles()) as unknown as Profiles;
+function Builders() {
+  const [builders, setbuilders] = useState<Profiles>();
 
-  console.log(builders);
+  const fetchSave = async () => {
+    let bd = (await fetchProfiles()) as unknown as Profiles;
+    setbuilders(bd);
+  };
+
+  useEffect(() => {
+    fetchSave();
+  }, []);
 
   return (
     <main className="flex w-[660px] min-h-screen flex-col items-center justify-between p-4 max-[900px]:p-0 max-[425px]:w-full">
       <div className="flex  gap-4 w-full flex-wrap justify-center">
-        {[...builders.profiles].map((ele, idx) => {
-          return (
-            <BuilderCard
-              key={idx + "builder"}
-              user={{
-                display_name: ele.author?.display_name as string,
-                pfp: ele.author?.pfp_url as string,
-                username: ele.author?.username as string,
-              }}
-              skill={{
-                designation: ele.data.raw_designation as string,
-                tags: (ele.data.raw_tags || "").split(",").map((sk) => {
-                  return sk.trim();
-                }),
-              }}
-            />
-          );
-        })}
+        {builders &&
+          [...builders.profiles].map((ele, idx) => {
+            return (
+              <BuilderCard
+                key={idx + "builder"}
+                user={{
+                  display_name: ele.author?.display_name as string,
+                  pfp: ele.author?.pfp_url as string,
+                  username: ele.author?.username as string,
+                }}
+                skill={{
+                  designation: ele.data.raw_designation as string,
+                  tags: (ele.data.raw_tags || "").split(",").map((sk) => {
+                    return sk.trim();
+                  }),
+                }}
+              />
+            );
+          })}
       </div>
     </main>
   );
